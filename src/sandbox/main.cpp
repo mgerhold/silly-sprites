341e90ad2c4bb43e5797a8d849afb87c37e95659
Michael Gerhold <michael.gerhold@web.de>
2023-07-24 20:50:47 +0200

    added sample code to demonstrate the use of some external libraries

--- src/sandbox/main.cpp
+++ src/sandbox/main.cpp
@@ -1,5 +1,34 @@
+#include <GLFW/glfw3.h>
+#include <entt/entt.hpp>
+#include <glad/gl.h>
+#include <glm/glm.hpp>
+#include <gsl/gsl>
 #include <iostream>
+#include <magic_enum/magic_enum_wrapper.hpp>
+#include <nlohmann/json.hpp>
+#include <spdlog/spdlog.h>
+#include <tl/expected.hpp>
+#include <tl/optional.hpp>
+
+enum class Color {
+    Red,
+    Green,
+    Blue,
+};
 
 int main() {
-    std::cout << "Hello, world!\n";
+    spdlog::info("Hello, world!");
+    auto vec = tl::optional<glm::vec3>{
+        glm::vec3{1.0, 2.0, 3.0}
+    };
+    if (vec.has_value()) {
+        spdlog::info("{},{},{}", vec->x, vec->y, vec->z);
+    }
+    const auto value = gsl::narrow_cast<float>(3.14);
+    spdlog::info("{}", value);
+    spdlog::info("this is just a test!");
+
+    for (const auto color : magic_enum::enum_values<Color>()) {
+        spdlog::info("{} is a color", magic_enum::enum_name(color));
+    }
 }
