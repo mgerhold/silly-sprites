c0c993115710139b6ee1bb0185b8c8ff110f7fcb
Martin Schreurs <martin.schreurs@netcologne.de>
2023-07-26 17:57:31 +0200

    create open cl directory

---
+++ src/sandboxpurpur/main.cpp
@@ -0,0 +1,17 @@
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <entt/entt.hpp>
+#include <glad/gl.h>
+#include <glm/glm.hpp>
+#include <gsl/gsl>
+#include <iostream>
+#include <magic_enum_wrapper.hpp>
+#include <nlohmann/json.hpp>
+#include <spdlog/spdlog.h>
+#include <test.hpp>
+#include <tl/expected.hpp>
+#include <tl/optional.hpp>
+
+int main() {
+
+}
