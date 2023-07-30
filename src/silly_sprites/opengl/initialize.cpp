9a4682bd2158d160c387947836497dd92de115b8
Martin Schreurs <martin.schreurs@netcologne.de>
2023-07-30 17:46:51 +0200

    testing

--- src/silly_sprites/opengl/initialize.cpp
+++ src/silly_sprites/opengl/initialize.cpp
@@ -18,8 +18,12 @@ namespace sly::gl {
             window->swap_buffers();
             glfwPollEvents();
             Input::update_keys(window.value());
+            if (Input::is_key_pressed(Keys::KeyA)) { spdlog::info("a is press"); }
             if (Input::is_key_down(Keys::KeyA)) { spdlog::info("a is down"); }
+            if (Input::is_key_hold(Keys::KeyA)) { spdlog::info("a is hold"); }
+            if (Input::is_key_released(Keys::KeyA)) { spdlog::info("a is release"); }
             if (Input::is_key_up(Keys::KeyA)) { spdlog::info("a is up"); }
+            if (Input::is_key_raised(Keys::KeyA)) { spdlog::info("a is raise"); }
         }
     }
 } // namespace sly::gl
