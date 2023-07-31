05486c06f8617e6899d712f0affea70ced7b08da
Martin Schreurs <martin.schreurs@netcologne.de>
2023-07-31 20:30:23 +0200

    clang format

--- src/silly_sprites/opengl/input.cpp
+++ src/silly_sprites/opengl/input.cpp
@@ -2,34 +2,34 @@
 
 namespace sly::gl {
     void Input::update_keys(Window const& window) {
-    
+
         std::swap(current_keys, last_keys);
         current_keys.fill(false);
-    
+
         for (int i = 0; i < current_keys.size(); ++i) {
             auto const key = static_cast<Key>(i);
             auto const g_key = glfwGetKey(window.get(), key_to_glfw(key));
-            if (g_key == GLFW_PRESS) { current_keys[i] = true; }
+            if (g_key == GLFW_PRESS) {
+                current_keys[i] = true;
             }
+        }
     }
     void Input::update_mouse(Window const& window) {
         std::swap(current_mouse, last_mouse);
         current_mouse.fill(false);
 
-        for (int i = 0; i < current_mouse.size(); ++i) { 
+        for (int i = 0; i < current_mouse.size(); ++i) {
             auto const key = static_cast<Mouse>(i);
             auto const g_key = glfwGetMouseButton(window.get(), mouse_to_glfw(key));
-            if (g_key == GLFW_PRESS) { current_mouse[i] = true; }
+            if (g_key == GLFW_PRESS) {
+                current_mouse[i] = true;
+            }
         }
     }
     void Input::update_mouse_position(Window const& window) {
-        double x,y;
+        double x, y;
         glfwGetCursorPos(window.get(), &x, &y);
-        mouse_position = {
-            static_cast<float>(x),
-            static_cast<float>(y)
-        };
-
+        mouse_position = { static_cast<float>(x), static_cast<float>(y) };
     }
 
     void Input::update(Window const& window) {
@@ -46,13 +46,14 @@ namespace sly::gl {
         return current_keys[static_cast<int>(key)];
     }
     [[nodiscard]] bool Input::is_key_up(Key const key) {
-        return not current_keys[static_cast<int>(key)];;
+        return not current_keys[static_cast<int>(key)];
+        ;
     }
     [[nodiscard]] bool Input::is_key_pressed(Key const key) {
-            return not last_keys[static_cast<int>(key)] and current_keys[static_cast<int>(key)];
+        return not last_keys[static_cast<int>(key)] and current_keys[static_cast<int>(key)];
     }
     [[nodiscard]] bool Input::is_key_released(Key const key) {
-            return last_keys[static_cast<int>(key)] and not current_keys[static_cast<int>(key)];
+        return last_keys[static_cast<int>(key)] and not current_keys[static_cast<int>(key)];
     }
 
     [[nodiscard]] bool Input::is_mouse_down(Mouse const mouse) {
@@ -62,9 +63,10 @@ namespace sly::gl {
         return not current_mouse[static_cast<int>(mouse)];
     }
     [[nodiscard]] bool Input::is_mouse_pressed(Mouse const mouse) {
-            return not last_mouse[static_cast<int>(mouse)] and current_mouse[static_cast<int>(mouse)];
+        return not last_mouse[static_cast<int>(mouse)] and current_mouse[static_cast<int>(mouse)];
     }
     [[nodiscard]] bool Input::is_mouse_released(Mouse const mouse) {
-            return last_mouse[static_cast<int>(mouse)] and not current_mouse[static_cast<int>(mouse)];
+        return last_mouse[static_cast<int>(mouse)] and not current_mouse[static_cast<int>(mouse)];
     }
-}
+
+} // namespace sly::gl
--- src/silly_sprites/opengl/input.hpp
+++ src/silly_sprites/opengl/input.hpp
@@ -1,43 +1,43 @@
 #pragma once
-#include "window.hpp"
 #include "key.hpp"
 #include "mouse.hpp"
+#include "window.hpp"
 #include <array>
-#include <magic_enum.hpp>
 #include <glm/glm.hpp>
+#include <magic_enum.hpp>
 
 namespace sly::gl {
-	class Input final {
-	private:
-		static constexpr auto num_key = magic_enum::enum_count<Key>(); 
-		static constexpr auto num_mouse = magic_enum::enum_count<Mouse>();
+    class Input final {
+    private:
+        static constexpr auto num_key = magic_enum::enum_count<Key>();
+        static constexpr auto num_mouse = magic_enum::enum_count<Mouse>();
 
-		static inline std::array<bool, num_key> current_keys{  };
-		static inline std::array<bool, num_key> last_keys{  };
+        static inline std::array<bool, num_key> current_keys{};
+        static inline std::array<bool, num_key> last_keys{};
 
-		static inline std::array<bool, num_mouse> current_mouse{  };
-		static inline std::array<bool, num_mouse> last_mouse{  };
+        static inline std::array<bool, num_mouse> current_mouse{};
+        static inline std::array<bool, num_mouse> last_mouse{};
 
-		using Vec2 = glm::vec2;
-		static inline Vec2 mouse_position{ }; 
+        using Vec2 = glm::vec2;
+        static inline Vec2 mouse_position{};
 
-		static void update_keys(Window const& window);
-		static void update_mouse(Window const& window);
-		static void update_mouse_position(Window const& window);
+        static void update_keys(Window const& window);
+        static void update_mouse(Window const& window);
+        static void update_mouse_position(Window const& window);
 
-	public:
-		static void update(Window const& window);
+    public:
+        static void update(Window const& window);
 
-		[[nodiscard]] static Vec2 get_mouse_position();
+        [[nodiscard]] static Vec2 get_mouse_position();
 
-		[[nodiscard]] static bool is_key_down(Key key);
-		[[nodiscard]] static bool is_key_up(Key key);
-		[[nodiscard]] static bool is_key_pressed(Key key);
-		[[nodiscard]] static bool is_key_released(Key key);
+        [[nodiscard]] static bool is_key_down(Key key);
+        [[nodiscard]] static bool is_key_up(Key key);
+        [[nodiscard]] static bool is_key_pressed(Key key);
+        [[nodiscard]] static bool is_key_released(Key key);
 
-		[[nodiscard]] static bool is_mouse_down(Mouse key);
-		[[nodiscard]] static bool is_mouse_up(Mouse key);
-		[[nodiscard]] static bool is_mouse_pressed(Mouse key);
-		[[nodiscard]] static bool is_mouse_released(Mouse key);
-	};
-}
+        [[nodiscard]] static bool is_mouse_down(Mouse key);
+        [[nodiscard]] static bool is_mouse_up(Mouse key);
+        [[nodiscard]] static bool is_mouse_pressed(Mouse key);
+        [[nodiscard]] static bool is_mouse_released(Mouse key);
+    };
+} // namespace sly::gl
