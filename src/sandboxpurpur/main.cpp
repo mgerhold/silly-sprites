90088149cd96ffe783dc00724fea6e1461c72ae6
Martin Schreurs <martin.schreurs@netcologne.de>
2023-08-06 13:51:59 +0200

    remove gl_initialize

--- src/sandboxpurpur/main.cpp
+++ src/sandboxpurpur/main.cpp
@@ -1,5 +1,18 @@
-#include "gl_initialize.hpp"
+#include "../silly_sprites/input.hpp"
+#include "../silly_sprites/opengl/window.hpp"
+#include "magic_enum_wrapper.hpp"
 
 int main() {
-    sly::gl::initialize(800, 600);
+    auto window = sly::gl::Window::create(800, 600);
+
+    if (not window.has_value()) {
+        spdlog::critical(magic_enum::enum_name(window.error()));
+        return 0;
+    }
+
+    while (not window->should_close()) {
+        window->swap_buffers();
+        glfwPollEvents();
+        sly::Input::update(window.value());
+    }
 }
