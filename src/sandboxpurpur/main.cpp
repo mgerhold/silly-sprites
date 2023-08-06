4898c41524ad5234ab6a0347011ca5586f55bd6c
Martin Schreurs <martin.schreurs@netcologne.de>
2023-08-06 14:12:48 +0200

    purple window

--- src/sandboxpurpur/main.cpp
+++ src/sandboxpurpur/main.cpp
@@ -1,6 +1,9 @@
+#define GLFW_INCLUDE_NONE
+
 #include "../silly_sprites/input.hpp"
 #include "../silly_sprites/opengl/window.hpp"
 #include "magic_enum_wrapper.hpp"
+#include <glad/gl.h>
 
 int main() {
     auto window = sly::gl::Window::create(800, 600);
@@ -10,7 +13,13 @@ int main() {
         return 0;
     }
 
+    // wrap this in some kind of rendering class
+    glClearColor(0.5f, 0.0f, 1.0f, 1.0f);
+
     while (not window->should_close()) {
+        // wrap this in some kind of rendering class
+        glClear(GL_COLOR_BUFFER_BIT);
+
         window->swap_buffers();
         glfwPollEvents();
         sly::Input::update(window.value());
