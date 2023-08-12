795aa27e7642bb4aa4b63f3247b437aa29188107
Michael Gerhold <michael.gerhold@web.de>
2023-08-12 19:50:12 +0200

    Enabled warnings, added sanitizers and improved overall CMake project structure
    Added a new 'sanitizers.cmake' file to enable GCC, Clang and MSVC sanitizers.
    This sets up 'address' and 'undefined behavior' sanitizers.
    
    Restructured setup of dependencies by moving it to a new file 'dependencies.cmake'.
    
    Modified 'project_options.cmake' to include the new sanitizers setup.
    
    To suppress warnings from these libraries, linked library targets are treated as system libraries.
    
    Added a new 'warnings.cmake' file providing an enabling function for warnings and treating warnings as errors, depending on a given flag.
    
    In 'silly_sprites/CMakeLists.txt', modified the linking of libraries and setup dependencies, thereby streamlining the dependencies config.
    
    In sum, these changes enhance the build setup and code safety, by leveraging sanitizers, warnings and efficient handling of dependencies.

---
+++ dependencies.cmake
@@ -0,0 +1,77 @@
+include("${CMAKE_SOURCE_DIR}/cmake/CPM.cmake")
+
+function(setup_dependencies)
+
+    CPMAddPackage(
+            NAME NLOHMANN_JSON
+            GITHUB_REPOSITORY nlohmann/json
+            VERSION 3.10.5
+            OPTIONS
+            "JSON_BuildTests OFF"
+            "JSON_CI OFF"
+    )
+    CPMAddPackage("gh:g-truc/glm#0.9.9.8")
+    CPMAddPackage(
+            NAME TL_OPTIONAL
+            GITHUB_REPOSITORY TartanLlama/optional
+            VERSION 1.1.0
+            OPTIONS
+            "OPTIONAL_BUILD_TESTS OFF"
+            "OPTIONAL_BUILD_PACKAGE OFF"
+            "DPKG_BUILDPACKAGE_FOUND OFF"
+            "OPTIONAL_BUILD_PACKAGE OFF"
+            "RPMBUILD_FOUND OFF"
+            "OPTIONAL_BUILD_PACKAGE OFF"
+            "CMAKE_HOST_WIN32 OFF"
+    )
+    CPMAddPackage(
+            NAME TL_EXPECTED
+            GITHUB_REPOSITORY TartanLlama/expected
+            VERSION 1.1.0
+            OPTIONS
+            "EXPECTED_BUILD_TESTS OFF"
+            "EXPECTED_BUILD_PACKAGE_DEB OFF"
+    )
+    CPMAddPackage(
+            NAME SPDLOG
+            GITHUB_REPOSITORY gabime/spdlog
+            VERSION 1.12.0
+            OPTIONS
+            "SPDLOG_BUILD_EXAMPLE OFF"
+            "SPDLOG_BUILD_TESTS OFF"
+    )
+    CPMAddPackage(
+            NAME ENTT
+            GITHUB_REPOSITORY skypjack/entt
+            VERSION 3.12.2
+            OPTIONS
+            "ENTT_USE_SANITIZER ON"
+            "ENTT_BUILD_TESTING OFF"
+            "ENTT_BUILD_DOCS OFF"
+    )
+    CPMAddPackage(
+            NAME GSL
+            GITHUB_REPOSITORY microsoft/GSL
+            VERSION 4.0.0
+            OPTIONS
+            "GSL_TEST OFF"
+    )
+    CPMAddPackage(
+            NAME MAGIC_ENUM
+            GITHUB_REPOSITORY Neargye/magic_enum
+            VERSION 0.9.3
+            OPTIONS
+            "MAGIC_ENUM_OPT_BUILD_EXAMPLES OFF"
+            "MAGIC_ENUM_OPT_BUILD_TESTS OFF"
+    )
+    CPMAddPackage(
+            NAME GLFW
+            GITHUB_REPOSITORY glfw/glfw
+            GIT_TAG 3.3.8
+            OPTIONS
+            "GLFW_BUILD_TESTS OFF"
+            "GLFW_BUILD_EXAMPLES OFF"
+            "GLFW_BULID_DOCS OFF"
+    )
+
+endfunction()
