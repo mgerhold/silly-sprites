f096af5bcf258afe71957741fb5f3630059f4541
Martin Schreurs <martin.schreurs@netcologne.de>
2023-10-07 18:14:41 +0200

    chance brackets

--- src/silly_sprites/opengl/error.cpp
+++ src/silly_sprites/opengl/error.cpp
@@ -1,9 +1,11 @@
 #include "error.hpp"
 
 namespace sly::gl {
-    GlError::GlError(GlErrorType const type) : GlError(type, "") { }
+    GlError::GlError(GlErrorType const type) : GlError{ type, "" } { }
 
-    GlError::GlError(GlErrorType const type, std::string const message) : m_type{ type }, m_message{ std::move(message) } {
+    GlError::GlError(GlErrorType const type, std::string const message)
+        : m_type{ type },
+          m_message{ std::move(message) } {
         auto const type_name = magic_enum::enum_name(m_type);
         if (m_message.empty()) {
             m_message = type_name;
