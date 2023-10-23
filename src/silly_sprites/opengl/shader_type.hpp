c16d26b5eb313b6203142642c8f03d40fc2c74ad
Martin Schreurs <martin.schreurs@netcologne.de>
2023-10-23 21:20:22 +0200

    add assert for invalid case and make it constexpr

--- src/silly_sprites/opengl/shader_type.hpp
+++ src/silly_sprites/opengl/shader_type.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <glad/gl.h>
 #include <string_view>
+#include <cassert>
 
 namespace sly::gl {
 
@@ -10,6 +11,17 @@ namespace sly::gl {
         Fragment = GL_FRAGMENT_SHADER,
     };
 
-    [[nodiscard]] std::string_view get_name_from_shader_type(ShaderType const type);
+    [[nodiscard]] constexpr std::string_view get_name_from_shader_type(ShaderType const type) {
+        switch (type) {
+            case ShaderType::Vertex:
+                return "VERTEX";
+            case ShaderType::Geometry:
+                return "GEOMETRY";
+            case ShaderType::Fragment:
+                return "FRAGMENT";
+        }
+        assert(false and "unreachable");
+        return "INVALID";
+    }
 
 } // namespace sly::gl
