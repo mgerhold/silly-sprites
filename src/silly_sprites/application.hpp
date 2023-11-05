09c735085362a4cbc512f05c665354f6a9fd2553
Michael Gerhold <michael.gerhold@web.de>
2023-11-05 16:16:30 +0100

    deleted copy and move operations for the `Application` class

--- src/silly_sprites/application.hpp
+++ src/silly_sprites/application.hpp
@@ -46,6 +46,11 @@ namespace sly {
     public:
         explicit Application(ApplicationSettings settings);
 
+        Application(Application const&) = delete;
+        Application(Application&&) = delete;
+        Application& operator=(Application const&) = delete;
+        Application& operator=(Application&&) = delete;
+
         ~Application() override;
 
         void run();
